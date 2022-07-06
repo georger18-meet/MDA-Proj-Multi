cee41dccdeda9284dec1b7c4e26691efe044d272
Chen5454 <74614370+Chen5454@users.noreply.github.com>
2022-07-06 23:18:05 +0300

    push
    push

--- MDA Multiplayer/Assets/ChenMultiplayerScripts/SpawnManager.cs
+++ MDA Multiplayer/Assets/ChenMultiplayerScripts/SpawnManager.cs
@@ -15,6 +15,7 @@ public class SpawnManager : MonoBehaviour
         Vector3 randomPos = new Vector3(Random.Range(_minX,_maxX),0.5f,Random.Range(_minZ,_maxZ));
         PhotonNetwork.Instantiate(_playerPrefab.name, randomPos, Quaternion.identity);
         PhotonNetwork.InstantiateRoomObject(_patientPrefab.name, _patientSpawner.position, _patientPrefab.transform.rotation);
-        PhotonNetwork.InstantiateRoomObject(NatanPrefab.name, NatanSpanwner.position, NatanPrefab.transform.rotation);
+        if (PhotonNetwork.IsMasterClient)
+            PhotonNetwork.InstantiateRoomObject(NatanPrefab.name, NatanSpanwner.position, NatanPrefab.transform.rotation);
     }
 }
