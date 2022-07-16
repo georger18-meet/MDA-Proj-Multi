97b366035b02875548c5c9df9f46aa362c72b2b2
George Rashed <goerge1110@gmail.com>
2022-07-16 16:20:18 +0300

    Crew Room UI Added with Crew Division Panel that sets Names of Player in Room.

--- MDA Multiplayer/Assets/Scripts/Managers/CrewRoomManager.cs
+++ MDA Multiplayer/Assets/Scripts/Managers/CrewRoomManager.cs
@@ -3,10 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class CrewRoomManager : MonoBehaviour
 {
     public GameObject RoomDoorBlocker;
+    public GameObject RoomCrewMenuUI;
+    public TextMeshProUGUI CrewMemberNameText1, CrewMemberNameText2, CrewMemberNameText3, CrewMemberNameText4;
 
     public List<PhotonView> _playersInRoomList;
     public int _playersMaxCount = 4;
@@ -16,12 +19,12 @@ public class CrewRoomManager : MonoBehaviour
     private void Awake()
     {
         _photonView = GetComponent<PhotonView>();
+        RoomCrewMenuUI.SetActive(false);
     }
 
     void Update()
     {
        // BlockRoomAccess();
-       
     }
 
 
@@ -52,17 +55,52 @@ public class CrewRoomManager : MonoBehaviour
         return playerFound;
     }
 
+    private void RefreshCrewNamesTexts()
+    {
+        for (int i = 0; i < _playersInRoomList.Count; i++)
+        {
+            switch (i)
+            {
+                case 0:
+                    CrewMemberNameText1.text = _playersInRoomList[i].name;
+                    break;                
+                case 1:
+                    CrewMemberNameText2.text = _playersInRoomList[i].name;
+                    break;                
+                case 2:
+                    CrewMemberNameText3.text = _playersInRoomList[i].name;
+                    break;                
+                case 3:
+                    CrewMemberNameText4.text = _playersInRoomList[i].name;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public void ShowCrewRoomMenu()
+    {
+        RoomCrewMenuUI.SetActive(true);
+        RefreshCrewNamesTexts();
+    }
+
+    public void HideCrewRoomMenu()
+    {
+        RoomCrewMenuUI.SetActive(false);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && _playersInRoomList.Count < _playersMaxCount && !CheckIfAlreadyInList(other.gameObject))
         {
            // _playersInRoomList.Add(other.gameObject.GetPhotonView());
-            _photonView.RPC("AddingToRoonList_RPC", RpcTarget.AllBufferedViaServer,PhotonNetwork.NickName);
+            _photonView.RPC("AddingToRoomList_RPC", RpcTarget.AllBufferedViaServer,PhotonNetwork.NickName);
         }
     }
 
     [PunRPC]
-    void AddingToRoonList_RPC(string currentPlayer)
+    void AddingToRoomList_RPC(string currentPlayer)
     {
 
         PhotonView currentPlayerData = GameObject.Find(currentPlayer).GetComponent<PhotonView>();
