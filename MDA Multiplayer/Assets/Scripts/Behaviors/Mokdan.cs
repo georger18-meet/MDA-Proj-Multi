d9062c6bce98b13ede073401340689c4ae74fe1e
Chen5454 <74614370+Chen5454@users.noreply.github.com>
2022-08-23 17:11:04 +0300

    Main Mokdan
    added Main Mokdan

--- MDA Multiplayer/Assets/Scripts/Behaviors/Mokdan.cs
+++ MDA Multiplayer/Assets/Scripts/Behaviors/Mokdan.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Mokdan : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] public bool isMainMokdan;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
