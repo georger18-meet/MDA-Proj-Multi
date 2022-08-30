475e35cd238e28103f1b02b6e393aed79c6b9716
Chen5454 <74614370+Chen5454@users.noreply.github.com>
2022-08-30 19:26:41 +0300

    Edit Henyon UI
    Edit Henyon UI

--- MDA Multiplayer/Assets/Scripts/Behaviors/Henyon10.cs
+++ MDA Multiplayer/Assets/Scripts/Behaviors/Henyon10.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Henyon10 : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
+
+    private bool _isHenyon10MenuOpen;
+    public Button TopMenuHandle,ShowCarsMenu;
+    public GameObject Henyon10Menu;
+
     void Start()
+    {
+        UIManager.Instance.TeamLeaderMenu.SetActive(false);
+        UIManager.Instance.Henyon10Menu.SetActive(true);
+
+
+        ShowCarsMenu = UIManager.Instance.Henyon10CarsMenu;
+        Henyon10Menu = UIManager.Instance.Henyon10Menu;
+        TopMenuHandle = UIManager.Instance.Henyon10MenuHandle;
+        TopMenuHandle.onClick.RemoveAllListeners();
+        TopMenuHandle.onClick.AddListener(delegate { OpenCloseHenyon10Menu(); });
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void OpenCloseHenyon10Menu()
     {
-        
+        if (!_isHenyon10MenuOpen)
+        {
+            UIManager.Instance.OpenCloseTopMenu("Henyon10");
+            _isHenyon10MenuOpen = true;
+        }
+        else
+        {
+            UIManager.Instance.OpenCloseTopMenu("Henyon10");
+            _isHenyon10MenuOpen = false;
+        }
+
+        TopMenuHandle.onClick.RemoveAllListeners();
+        TopMenuHandle.onClick.AddListener(delegate { OpenCloseHenyon10Menu(); });
     }
 }
